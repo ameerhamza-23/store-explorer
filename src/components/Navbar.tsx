64c439c223de4a3d87aef6378e05058ca2bd00d5
ameerhamza-23 <hamzaiscoding@gmail.com>
2025-04-23 23:47:01 +0500

    chore: add cart count in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="h-16 bg-blue-300 flex gap-4 justify-center items-center">
+        <nav className="h-16 bg-blue-300 flex items-center justify-between">
+
+            <div className="p-4 flex gap-4">
                 <Link to="/">Home</Link>
                 <Link to="/cart">Cart</Link>
+            </div>
+
+            <div className="p-4">
+                count 0
+            </div>
+
         </nav>
     )
 }
