c55fdb88a5a3aff568a75ba92c412afddaadd37e
ameerhamza-23 <hamzaiscoding@gmail.com>
2025-04-23 18:38:22 +0500

    chore: add main layout component

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "./Navbar"
+
+export default function MainLayout() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <div className="flex-1">
+                <Outlet />
+            </div>
+        </div>
+    )
+}
