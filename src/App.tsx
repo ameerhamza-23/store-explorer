135653e2bc716f68ca7103d432bfc5adeb4b6e7a
ameerhamza-23 <hamzaiscoding@gmail.com>
2025-04-23 18:49:01 +0500

    chore: wrap pages in the layout component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CartPage from './pages/CartPage'
-import Navbar from './components/Navbar'
+import MainLayout from './components/MainLayout'
 
 function App() {
 
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/cart" element={<CartPage />}/>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/cart" element={<CartPage />}/>
+        </Route>
       </Routes>
     </Router>
   )
