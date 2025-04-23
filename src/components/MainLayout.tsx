a1f73d877651d9632f48307ccd8c770794025d3d
ameerhamza-23 <hamzaiscoding@gmail.com>
2025-04-23 19:18:28 +0500

    bugfix: fix layout issue

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -5,7 +5,7 @@ export default function MainLayout() {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <div className="flex-1">
+            <div className="flex flex-1">
                 <Outlet />
             </div>
         </div>
