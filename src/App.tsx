19060282e6971231a92ac0a345e7c6f6afacd8a6
ameerhamza-23 <hamzaiscoding@gmail.com>
2025-04-23 17:55:40 +0500

    chore: add react router dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CartPage from './pages/CartPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1> <h1></h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/cart" element={<CartPage />}/>
+      </Routes>
+    </Router>
   )
 }
 
