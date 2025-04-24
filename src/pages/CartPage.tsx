79103c56c6bfc91582753af0b3ee32e271909660
ameerhamza-23 <hamzaiscoding@gmail.com>
2025-04-25 00:04:58 +0500

    chore: display subtotal of products added to cart

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,18 +1,28 @@
-import Header from "../components/home/Header";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../reducers";
 import { Product } from "../types/Product";
-import { useState } from "react";
 import { RemoveFromCart } from "../actions/cartActions";
+import { useState, useEffect } from "react";
 
 export default function CartPage() {
 
   const dispatch = useDispatch();
   const products = useSelector((state: RootState) => state.cart.cart);
-  const [searchTerm, setSearchTerm] = useState('')
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    setTotal(
+      products.reduce((acc, product) => acc + product.price, 0)
+    );
+  }, [products]);
 
   return (
     <div className="w-full h-full m-8 overflow-x-auto">
+
+        <div className="flex justify-end">
+            <h1 className="text-sm">Subtotal: <span className="font-semibold">{total}</span></h1>
+        </div>
+
       <table className="min-w-full border border-gray-300 text-sm mt-8">
         <thead className="bg-gray-100 text-left">
           <tr>
